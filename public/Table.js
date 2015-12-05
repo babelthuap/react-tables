adb55df5d53a6737859943be356f1179e9108dda
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-12-04 20:32:42 -0800

    Display table data in honest-to-goodness tables

--- public/Table.js
+++ public/Table.js
@@ -1,16 +1,36 @@
 import React from "react";
 
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headers: Object.keys(this.props.data[0])
+    };
+  }
   componentDidMount() {
-    console.log('table mounted')
+    console.log( 'this.props.data:', this.props.data )
   }
   render() {
     return (
       <div>
-        {this.props.data}
+        <table>
+          <thead>
+            <tr>
+              { this.state.headers.map((header, i) => <th key={i}>{header}</th>) }
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.data.map((datum, i) => {
+              return <tr key={i}>
+                { this.state.headers.map((header, j) => <td key={j}>{datum[header]}</td>) }
+              </tr>
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
 export default Table;
+// {this.props.data}
--- public/script.js
+++ public/script.js
@@ -4,15 +4,13 @@ import ReactDOM from "react-dom";
 import Table from "./Table";
 import tableData from "./data";
 
-console.log('tableData:', tableData);
-
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Table data={ JSON.stringify(tableData[0]) } />
+        <Table data={tableData[0]} />
         <hr />
-        <Table data={ JSON.stringify(tableData[1]) } />
+        <Table data={tableData[1]} />
       </div>
     );
   }
